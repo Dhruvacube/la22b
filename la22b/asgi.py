a8fa178263563fd8a7c4294e906794fe57247c6b
Dhruva Shaw <dhruvashaw@gmail.com>
2020-12-31 08:45:29 +0530

    rename farewell sc-1 folder to la22b

--- la22b/asgi.py
+++ la22b/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farewellsc1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'la22b.settings')
 
 application = get_asgi_application()
